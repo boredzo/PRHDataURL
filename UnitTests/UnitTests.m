9527b21072f6be65ab77aa9a8ea96cbe1b5f149a
Mike Abdullah <me@mikeabdullah.net>
2013-02-27 15:39:02 +0000

    Add project for unit testing

---
+++ UnitTests/UnitTests.m
@@ -0,0 +1,18 @@
+//
+//  UnitTests.m
+//  UnitTests
+//
+//  Created by Mike on 27/02/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "NSData+PRHDataURL.h"
+
+@interface UnitTests : SenTestCase
+
+@end
+
+@implementation UnitTests
+
+@end
